87a7a9b07440b7f90ddf3e955e2a313a965d93b2
Chetan-Vikasit <chetan.singh@vikasit.com>
2023-07-14 17:49:39 +0530

    form taking input data and printing to console

--- app/routes/form.tsx
+++ app/routes/form.tsx
@@ -1,31 +1,52 @@
+import { log } from "node:console";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+
 export default function form() {
+  const [startDate, setStartDate] = useState(null);
+  const [name, setname] = useState("");
+  const [lan, setlan] = useState("");
+
   function onChangedate(date: any) {
     setStartDate(date);
   }
-  const [startDate, setStartDate] = useState(null);
+
+  function changeHanderButton(event: any) {
+    event.preventDefault();
+    console.log(
+      `names: ${name}`,
+      `Date of Birth:${startDate}`,
+      `languages:${lan}`
+    );
+  }
+
   return (
     <>
-      <div className="formContainer px-4">
-        <h1 className="py-0.5 pt-6 text-2xl">Account</h1>
+      <div className="formContainer justify-center px-4 py-6">
+        <h1 className="py-0.5 pt-6 text-2xl ">Account</h1>
         <p className="pb-6 text-gray-500">
           Update your account settings. Set your preferred language and
           timezone.
         </p>
-        <hr className="w-2/3 pb-2" />
-        <form className="block focus:outline-none">
+        <hr className="w-1/2 pb-2" />
+        <form
+          className="items-center justify-center focus:outline-none"
+          onSubmit={changeHanderButton}
+        >
           <div className="grid gap-2">
             <label className="text-lg " htmlFor="Name">
               Name
             </label>
             <input
-              className="w-2/3 rounded-lg border py-2 outline-none"
+              className="w-1/2 rounded-lg border py-2 outline-none"
               type="text"
-              name=""
+              name="Name"
+              value={name}
+              onChange={(e) => setname(e.target.value)}
               id="Name"
-              placeholder="  Your Name"
+              placeholder="Your Name"
+              autoComplete="off"
             />
             <p className="text-l pb-6 text-gray-500">
               This is the name that will displayed on your profile and in
@@ -42,9 +63,10 @@ export default function form() {
                 className=" outline-none"
                 selected={startDate}
                 onChange={onChangedate}
-                placeholderText="  Pick a date "
+                placeholderText="Pick a date "
                 id="Date"
               />
+
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -60,7 +82,7 @@ export default function form() {
                 />
               </svg>
             </div>
-           <p className="text-l pb-6 text-gray-500">
+            <p className="text-l pb-6 text-gray-500">
               Your date of birth is used to calculate your age.
             </p>
           </div>
@@ -69,15 +91,51 @@ export default function form() {
               Languages
             </label>
 
-            <div className="flex w-80 rounded-lg border py-2  outline-none">
-            <select className="flex w-64 rounded-lg py-1 outline-none bg-white text-l px-2 text-gray-500" id="cars" name="carlist" form="carform">
-  <option value="volvo">English</option>
-  <option value="saab">Hindi</option>
-  <option value="opel">Marathi</option>
-  <option value="audi">Guzrati</option>
-</select>
+            <div className="flex w-64 rounded-lg border px-4 py-2 outline-none">
+              <select
+                className="text-l flex w-64 rounded-lg bg-white px-2 py-1 text-gray-500 outline-none"
+                id="lan"
+                name="carlist"
+                form="carform"
+                // onChange={changeHanderLanguage}
+                value={lan}
+                onChange={(e) => setlan(e.target.value)}
+              >
+                <option value="English">English</option>
+                <option value="Hindi">Hindi</option>
+                <option value="Marathi">Marathi</option>
+                <option value="Guzrati">Guzrati</option>
+              </select>
+            </div>
+            <p className="text-l pb-6 text-gray-500">
+              This is the language that will be used in the dashboard.
+            </p>
+          </div>
+          <button
+            className="w-40 rounded-lg border bg-black py-2 text-white"
+            type="submit"
+            onChange={changeHanderButton}
+          >
+            Update Account
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
+//  function changeHanderName(event:any) {
+//     const names =(event.target.value)
+//     setname(names);
+//     console.log(names);
+//   }
 
-              {/* <svg  
+//   function changeHanderLanguage(event:any) {
+//     const lanx =(event.target.value)
+//     setlan(lanx);
+//     console.log(lanx);
+//   }
+{
+  /* <svg  
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -90,17 +148,16 @@ export default function form() {
                   strokeLinejoin="round"
                   d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
                 />
-              </svg> */}
-            </div>
-            <p className="text-l pb-6 text-gray-500">
-              This is the language that will be used in the dashboard.
-            </p>
-          </div>
-          <button className="w-40 rounded-lg border bg-black py-2 text-white">
-            Update Account
-          </button>
-        </form>
-      </div>
-    </>
-  );
+              </svg> */
 }
+// const [user,setUser] = useState({
+//   name:"",DateOfBirth:null,Languages:""
+// });
+// let name,value;
+// const handleInputs=(event:any) =>
+// {
+//   const name =event.target.name;
+//   const  value =event.target.value;
+
+//    setUser({...user,[name]:value});
+// }
